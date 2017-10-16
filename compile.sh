8513f0944ccba3cd6ad01cac4581d9c5d44b9581
Cameron Nichols <cnic3139@uni.sydney.edu.au>
2017-10-16 16:04:15 +1100

    Fixed some errors

--- compile.sh
+++ compile.sh
@@ -9,11 +9,11 @@ if [ ${#} != 1 ]; then
 fi
 
 # builds the ANTLR-generated parse using the grammar file
-java -cp antlr-4.7.jar org.antlr.Tool -o ${BUILD_DIR} VPL.g
+java -jar /usr/local/lib/antlr-4.7-complete.jar -Dlanguage=Python3 -o ${BUILD_DIR} VPL.g
 touch ${BUILD_DIR}/__init__.py
 
 # uses the ANTLR-generated parser to convert the VPL program to ASM
-./vpl2asm.py < ${1} > {1}.s
+./vpl2asm.py ${1} > {1}.s
 
 # compiles the ASM and C file together
 gcc -Wall -W main.c ${1}.s -o my_program
--- vpl2asm.py
+++ vpl2asm.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import sys
-import antlr3
+import antlr4
 from build.VPLLexer import VPLLexer
 from build.VPLParser import VPLParser
 
-char_stream = antlr3.ANTLRInputStream(sys.stdin)
-lexer = VPLLexer(char_stream)
-tokens = antlr3.CommonTokenStream(Lexer)
-parser = VPLParse(tokens)
-root = parser.prog()
+
+def main(argv):
+    char_stream = antlr4.FileStream(argv[1])
+    lexer = VPLLexer(char_stream)
+    tokens = antlr4.CommonTokenStream(lexer)
+    parser = VPLParser(tokens)
+    root = parser.start()
+
+if __name__ == '__main__':
+    main(sys.argv)
