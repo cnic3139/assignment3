699716f771bc95a0d0bd9194b2bddd7366b3cf55
Cameron Nichols <cnic3139@uni.sydney.edu.au>
2017-10-22 14:33:33 +1100

    more work

--- my.py
+++ my.py
@@ -22,12 +22,24 @@ nodes = {}
 vals = {}
 valCounter = 0
 
+pars = []
+varis = []
+consts = []
+
 
 def getVal():
-    global v
+    global valCounter
     returnVal = valCounter
     valCounter += 1
     return returnVal
+
+
+def getDependencies(num):
+    returnList = []
+    for i in range(num):
+        returnList.append(getVal())
+        vals[returnList[-1]] = None
+    return returnList
 
 
 def dispatcher(node, direct, ctx):
@@ -68,16 +80,17 @@ def enter(node, ctx):
     myList = []
     # This is where you will store your value
     # Try and get it from parent or make it yourself
-    parentDeps = nodes[ctx.parentCtx][1]
     myNum = None
-    for dep in parentDeps:
-        if vals[dep] is None:
-            myNum = dep
-            break
+    if ctx.parentCtx is not None:  # To deal with root node
+        parentDeps = nodes[ctx.parentCtx][1]
+        for dep in parentDeps:
+            if vals[dep] is None:
+                myNum = dep
+                break
     if myNum is None:
         myNum = getVal()
     myList.append(myNum)  # myList[0] is where value is located
-    vals[myList[-1]] = None
+    vals[myList[-1]] = None  # Initialise with 'None' value
     # General pre-stuff goes here =============================================
 
     # Node-specific stuff - use selector to defer to nodes ====================
@@ -132,6 +145,13 @@ def selector(node, direct, ctx, list):
 def start(direct, ctx, list):
     myList = list
     if direct == "enter":
+        # Reinitialise objects
+        nodes = {}
+        vals = {}
+        pars = []
+        varis = []
+        consts = []
+        addVar("__TEMP__")  # Special variable for calculations
         pass
     elif direct == "exit":
         # THIS IS WHERE THE WHOLE PROGRAM IS GONNA BE BUILT?
@@ -153,33 +173,60 @@ def f(direct, ctx, list):
     if direct == "enter":
         pass
     elif direct == "exit":
+        # Declare memory for vars and consts
         pass
     return myList
+
+inP = False
 
 
 def p(direct, ctx, list):
     myList = list
     if direct == "enter":
+        inP = True
         pass
     elif direct == "exit":
+        inP = False
         pass
     return myList
+
+
+def addPar(par):
+    if par not in pars:
+        pars.append(par)
+
+
+def addVar(var):
+    if var not in varis:
+        varis.append(var)
+
+
+def addConst(const):
+    if const not in consts:
+        consts.append(const)
 
 
 def l(direct, ctx, list):
     myList = list
     if direct == "enter":
-        pass
+        if inP:
+            addPar(ctx.start.text)
+        elif inD:
+            addVar(ctx.start.text)
     elif direct == "exit":
         pass
     return myList
+
+inD = False
 
 
 def d(direct, ctx, list):
     myList = list
     if direct == "enter":
+        inD = True
         pass
     elif direct == "exit":
+        inD = False
         pass
 
 
@@ -204,13 +251,38 @@ def r(direct, ctx, list):
 def e(direct, ctx, list):
     myList = list
     if direct == "enter":
-        # This is where you will store your dependencies
-        myList.append([])  # myList[1] is list of where dependencies are
-        myList[-1].append(getVal())
-        vals[myList[-1][-1]] = None
-        myList[-1].append(getVal())
-        vals[myList[-1][-1]] = None
-        myList.append(lambda x, y: x + y)  # myList[2] is how to calc value
+
+        # Need to find out which branch to follow
+        text = ctx.start.text
+        if text == "add":
+            # This is where you will store your dependencies
+            # myList[1] is list of dependencies
+            myList.append(getDependencies(2))
+            myList.append("op")  # myList[2] is how to calc value
+        elif text == "minus":
+            myList.append(getDependencies(2))
+            myList.append("op")
+        elif text == "mult":
+            myList.append(getDependencies(2))
+            myList.append("op")
+        elif text == "div":
+            myList.append(getDependencies(2))
+            myList.append("op")
+        elif text == "min":
+            myList.append(getDependencies(2))
+            myList.append("op")
+        elif text == "(":
+            myList.append(getDependencies(1))
+            myList.append("e")
+        elif text.isnumeric():
+            # text == "num"
+            myList.append(getDependencies(1))
+            myList.append("num")
+            addConst(text)
+        else:
+            # text == "ident"
+            myList.append(getDependencies(1))
+            myList.append("ident")
 
     elif direct == "exit":
         # Lookup in node, get data from when you entered this node
@@ -219,7 +291,32 @@ def e(direct, ctx, list):
         # Now we should have all dependents accounted for, and we can
         #  get the value of this node and put it in the right place.
 
-        # Now i have all my dependents set, i can write my stuff
+        # Check myData[2], then pop it and replace it with code snippet?
+        if myData[2] == "op":
+            file = open("t_ident_=_op(factor,_factor).asm", "r")
+            template = file.read()
+            file.close()
+            template.replace("<load-source1-rax>", "")
+            template.replace("<load-source2-r10>", "")
+            template.replace("<load-dest-r11>", "")  # Wait until S???
+            template.replace("<operation>", "")
+            template.replace("<X>", "")
+        elif myData[2] == "e":
+            pass  # I dunno what to do here, just give all its stuff to parent?
+        elif myData[2] == "num":
+            file = open("t_ident_=_factor.asm", "r")
+            template = file.read()
+            file.close()
+            template.replace("<load-rax>", "")
+            template.replace("<load-r10>", "")  # Wait until S???
+            template.replace("<X>", "")
+        elif myData[2] == "ident":
+            file = open("t_ident_=_factor.asm", "r")
+            template = file.read()
+            file.close()
+            template.replace("<load-rax>", "")
+            template.replace("<load-r10>", "")  # Wait until S???
+            template.replace("<X>", "")
 
     return myList
 
